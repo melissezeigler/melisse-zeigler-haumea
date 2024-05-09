ed60167fd21f1b350a29ed32f473df76c7dc6c86
Melisse Zeigler <melissebernadas@gmail.com>
2024-05-09 01:03:54 -0800

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -18,3 +18,37 @@ for (let skill of skills) {
     skillItem.innerText = skill;
     skillsList.appendChild(skillItem);
 }
+
+// Lesson 14
+const messageForm = document.querySelector("[name = 'leave_message']");
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log(usersName);
+    console.log(usersEmail);
+    console.log(usersMessage);
+
+    const messageSection = document.querySelector("#messages");
+    const messageList = messageSection.querySelector("ul");
+    console.log(messageSection);
+
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href=mailto:${usersEmail}>${usersName}</a>
+    <span>${usersMessage}</span>
+    `;
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", (event) => {
+        const entry = event.target.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+});
